c15b20888e5b7694414da569fc18573a9e55b67e
noureddein <noureddein@gmail.com>
2022-04-19 17:49:33 +0300

    Adding menu

--- Canteen-project/canteen_project/canteen_pos.py
+++ Canteen-project/canteen_project/canteen_pos.py
@@ -1,36 +1,76 @@
-from Store import Store
-class CanteenSystem():
+menu = {
+    "Hot food": [
+        # H11
+        {
+            'meal 1': {
+                'desc': 'fresh soups with wholemeal bread rolls on the side',
+                'price': 2.0
+            }
+        },
+        {
+            'meal 2': {
+                'desc': 'pasta or lasagne with fresh tomato, bolognaise or vegetable-based sauce',
+                'price': 2.5
+            }
+        }
+    ],
+    'Snacks': {
+
+    },
+    "Drinks": [
+        {
+            'drink 1': {
+                'desc': 'Apple',
+                'price': 0.35
+            }
+        },
+        {
+            'drink 2': {
+                'desc': 'Orange',
+                'price': 0.35
+            },
+        }
+    ]
+}
+
+"""
+*  Choose your item  *
+*  1- meal 1: fresh soups with wholemeal bread rolls on the side, Price: 0.2 JOD  *
+*  2- Item: 2, Price: 0.5 JOD  *
+*  3- Item: 3, Price: 0.1 JOD  *
+*  4- Item: 4, Price: 0.35 JOD *
+
+"""
+
+
+class CanteenSystem:
+
+    def __init__(self):
+        pass
+
     @staticmethod
     def greeting():
-        print(
-'''===================================
+        greet = (
+            '''===================================
    Welcome to the Canteen System
 ===================================   
               ''')
-        
-        
-    
-    def __init__(self):
-        pass
-    
+        print(greet)
+        return greet
+
     def menu(self):
         Store.get_items()
         """_summary_
         """
         pass
-    
-    
-    
-    
-    def order():
+
+    def order(self):
         pass
-    
-    
-    
-    def recipe():
+
+    def recipe(self):
         pass
-    
-    
-    
+
+
 if __name__ == '__main__':
+    from Store import Store
     CanteenSystem.greeting()
--- Canteen-project/canteen_project/parent_portal.py
+++ Canteen-project/canteen_project/parent_portal.py
@@ -1,26 +1,37 @@
-class Operations():
+import json
+
+
+class Operations:
     def __init__():
         pass
-    
+
     def recharge():
         pass
+
     def max_daily_credit():
-        pass 
-        
+        pass
 
 
-class ParentPortal():
+class ParentPortal:
     def __init__(self):
-        pass 
-       
-    def buy_daily_meal():
-        pass 
-    
-    def not_allowed_items():
-        pass 
-    
-    def quit():
-        pass    
-        
-    
-    
+        pass
+
+    def _get_students_data(self):
+        with open('../Student_info.json', 'r') as f:
+            data = json.load(f)
+        return data
+
+    def buy_daily_meal(self):
+        pass
+
+    def not_allowed_items(self):
+        pass
+
+    def quit(self):
+        pass
+
+
+if __name__ == '__main__':
+    # data = ParentPortal._get_students_data()
+    # print(data[0])
+    pass
